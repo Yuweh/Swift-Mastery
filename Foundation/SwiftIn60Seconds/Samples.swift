e398c1a7eb32982dbdac94eb063bafb490fc6367
Jay <Yuweh@users.noreply.github.com>
2019-09-05 23:35:51 +0800

    arithmetics updated

--- Foundation/SwiftIn60Seconds/Samples.swift
+++ Foundation/SwiftIn60Seconds/Samples.swift
@@ -302,8 +302,15 @@ let earth = Planet(rawValue: 2) // ->Venus because starting count now is 1 inste
 * Arithmetic Operators
 
 /*
+Swift has operators for doing arithemetic and for comparison;
+they mostly work like you already know.
+
 */
 // Example:
+ let total = firstScore + secondScore
+ let difference = firstScore - secondScore
+ let product = firstScore * secondScore
+ let divided = firstScore / secondScore
 
 
 //====================================================================================
@@ -318,9 +325,12 @@ let earth = Planet(rawValue: 2) // ->Venus because starting count now is 1 inste
 * Compound assignment operators
 
 /*
+There are compound variants of arithmethic operators 
+that modify their variables in place: +=, -=, and so on.
 */
 // Example:
-
+ var score = 95
+ score -= 5
 
 //====================================================================================
 * Comparison operators
@@ -334,9 +344,14 @@ let earth = Planet(rawValue: 2) // ->Venus because starting count now is 1 inste
 * Conditions
 
 /*
+ You can use if, else, and else if to run code based on the result of a condition
 */
 // Example:
-
+if firstCard + secondCard == 21 {
+ print("Black Jack")
+} else {
+ print("Regular Cards")
+}
 
 //====================================================================================
 * Combining operators
@@ -350,24 +365,48 @@ let earth = Planet(rawValue: 2) // ->Venus because starting count now is 1 inste
 * The ternary operator
 
 /*
+Swift has a ternary operator that combines a check with true and false code blocks. 
+Although you might see it in other code, not recommended using it
 */
 // Example:
+let 1sthand = 11
+let 2ndhand = 10
 
+print(firsthand == secondhand ? "same" : "not")
 
 //====================================================================================
 * Switch statements
 
 /*
+If you have multiple conditions using the same value,
+use switch for clarity
 */
 // Example:
+switch weather {
+ case "rain":
+ 
+ case "sunny":
+ 
+ default:
+}
 
 
 //====================================================================================
 * Range operators
 /*
+
+You can make ranges using ..< and ... depending on
+whether the last number should be excluded or included.
+
 */
 // Example:
-
+switch score {
+ case 0..<50:
+ 
+ case 50..<85:
+ 
+ default:
+}
 
 //====================================================================================
  
