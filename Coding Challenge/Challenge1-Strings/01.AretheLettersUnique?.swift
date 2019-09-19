76b05b0ab4959ef42d7c285b032b84827efb70d9
Jay <Yuweh@users.noreply.github.com>
2019-09-19 09:54:45 +0800

    (っ˘▽˘)っ updated ⊂(◕.◕⊂)

---
+++ Coding Challenge/Challenge1-Strings/01.AretheLettersUnique?.swift
@@ -0,0 +1,20 @@
+// 1: Are the letters unique?
+
+/*
+Write a function that accepts a String as its only parameter, and returns true if the string has
+only unique letters, taking letter case into account.
+
+Sample input and output
+• The string “No duplicates” should return true.
+• The string “abcdefghijklmnopqrstuvwxyz” should return true.
+• The string “AaBbCc” should return true because the challenge is case-sensitive.
+• The string “Hello, world” should return false because of the double Ls and double Os.
+*/
+
+
+func letterChecker (_ letter: String) -> Bool {
+1. should return true or false
+2. check for duplicates
+3. should be case sensitive
+}
+
