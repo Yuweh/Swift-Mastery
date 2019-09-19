edf75029d3e6ee0aa6d3ed84b07f8f53580857bd
Jay <Yuweh@users.noreply.github.com>
2019-09-19 10:03:45 +0800

    (っ˘▽˘)っ updated ⊂(◕.◕⊂)

---
+++ Coding Challenge/Challenge1-Strings/02.IsAstringaPalindrome.swift
@@ -0,0 +1,23 @@
+//Challenge 2: Is a string a palindrome?
+
+/*
+
+Write a function that accepts a String as its only parameter, and returns true if the string
+reads the same when reversed, ignoring case.
+
+Sample input and output
+• The string “rotator” should return true.
+• The string “Rats live on no evil star” should return true.
+• The string “Never odd or even” should return false; even though the letters are the
+same in reverse the spaces are in different places.
+• The string “Hello, world” should return false because it reads “dlrow ,olleH”
+backwards.
+
+*/
+
+func stringChecker (_ string: String) {
+
+
+}
+
+
