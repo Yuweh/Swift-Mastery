1cf24bdefc04c65daa366da823d81f7227327aa6
Jay <Yuweh@users.noreply.github.com>
2019-11-06 08:48:07 +0800

    (っ˘▽˘)っ updated ⊂(◕.◕⊂)

---
+++ ARKit/Basics/Default.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Demo001
+//
+//  Created by Francis Jemuel Bergonia on 11/5/19.
+//  Copyright © 2019 Arkray Marketing, Inc. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = false
+        
+        // Show scene debug options
+        sceneView.debugOptions = [.showFeaturePoints]
+        
+        // Add lightSouce or make scene 3D enabled
+        sceneView.automaticallyUpdatesLighting = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+
+}
